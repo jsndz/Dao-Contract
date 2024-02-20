e1610acd70cb8a4faa1c6856706a15d85115c294
jsndz <jaisondz9360@gmail.com>
2024-02-21 00:11:03 +0530

    added functions

--- contracts/1_Dao.sol
+++ contracts/1_Dao.sol
@@ -17,7 +17,7 @@ contract Dao{
     mapping(address=>uint) public numOfshares;
     mapping(address=>mapping (uint=>bool)) public isVoted;
      mapping(address=>mapping (address =>bool)) public withdrawlStatus;
-     mapping (address=>uint) public proposals;
+     mapping (uint=>Proposal) public proposals;
      address[] public InvestorList;
     uint public  totalshares;
      uint public  availableFunds;
@@ -29,7 +29,7 @@ contract Dao{
 
     constructor(uint _contributionTimeEnd, uint _voteTime, uint _quorum){
         require(_quorum>0 && _quorum<100,"Not a valid value");
-        contributionTimeEnd = _contributionTimeEnd;
+        contributionTimeEnd = block.timestamp +_contributionTimeEnd;
         quorum = _quorum;
         voteTime = _voteTime;
         manager = msg.sender;
@@ -37,14 +37,56 @@ contract Dao{
     }
 
 
-        modifier onlyInvestor(){
-            require(isInvestor[msg.sender]==true,"Not an investor");
-            _;
+    modifier onlyInvestor(){
+        require(isInvestor[msg.sender]==true,"Not an investor");
+        _;
+    }
+    modifier onlyManager(){
+        require(manager==msg.sender,"Not an manager");
+        _;
+    }
+    function contribution() public payable  {
+        require(contributionTimeEnd>=block.timestamp,"Contribution time ended");
+        require(msg.value>0,"Send more than 0 ether");
+        isInvestor[msg.sender]= true;
+        numOfshares[msg.sender] = numOfshares[msg.sender] + msg.value;
+        totalshares= totalshares +msg.value;
+        availableFunds+= msg.value;
+        InvestorList.push(msg.sender);
+    }
+    function redeemShares(uint amount) public onlyInvestor(){
+        require(numOfshares[msg.sender]<=amount,"Not enough shares");
+        require(availableFunds<=amount,"Not enough funds");
+        numOfshares[msg.sender]-=amount;
+        if(numOfshares[msg.sender]==0){
+            isInvestor[msg.sender]=false;
         }
-
-
-        modifier onlyManager(){
-            require(manager==msg.sender,"Not an manager");
-            _;
+        availableFunds-=amount;
+        payable (msg.sender).transfer(amount);
+    }
+    function transferShare(uint amount, address to) public  onlyInvestor() {
+        require(numOfshares[msg.sender]<=amount,"Not enough shares");
+        require(availableFunds<=amount,"Not enough funds");
+        numOfshares[msg.sender]-=amount;
+        if(numOfshares[msg.sender]==0){
+            isInvestor[msg.sender]=false;
         }
+        numOfshares[to]+=amount;
+        isInvestor[to]=true;
+        InvestorList.push(to);
+    }
+    function createProposal(string calldata description,uint amount,address payable recipient) public onlyManager(){
+        require(availableFunds<=amount,"Not enough funds");
+        proposals[nextProposalId]=Proposal(nextProposalId,description,amount,recipient,0,block.timestamp+voteTime,false);
+        nextProposalId++;
+    }
+
+    function voteProposal(uint proposalId) public onlyInvestor(){
+        Proposal storage proposal =proposals[proposalId];
+        require(isVoted[msg.sender][proposalId]==false,"You have already voted");
+        require(proposal.end>=block.timestamp,"Voting Time Ended");
+        require(proposal.executed==false,"It is already executed");
+        isVoted[msg.sender][proposalId]=true;
+        proposal.votes+=numOfshares[msg.sender];
+    }
 }
